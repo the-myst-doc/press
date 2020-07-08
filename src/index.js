620adcf2accb81f5887577586f10d22305141a01
Elana Bell Bogdan <e.bell.bogdan@gmail.com>
2020-07-08 17:44:13 -0400

    going in circles here, but disabling the vh trick...

--- src/index.js
+++ src/index.js
@@ -24,8 +24,8 @@ $(document).ready(() => {
     const bottomLink$ = pageContent$.find('a').last();
 
     function updateSpacers() {
-        const vh = window.innerHeight * 0.01;
-        document.documentElement.style.setProperty('--vh', `${vh}px`);
+        // const vh = window.innerHeight * 0.01;
+        // document.documentElement.style.setProperty('--vh', `${vh}px`);
 
         const heightDelta = bottomLink$.position().top - rand$.position().top;
         $('.spacer.dynamic').css('height', heightDelta / 2 - 410);
